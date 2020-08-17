d0efb679c01d3987e76fa30cd2ee34e749a6649f
Camilo Suaza <camilo.suaza@talosdigital.com>
2020-08-17 01:21:58 -0500

    Passing data between ViewControllers (LeagueVC to SkillVC) adding a model which contains a struct. The button next is enabled when some desired league is selected.

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Camilo Suaza on 17/08/20.
+//  Copyright © 2020 Camilo Suaza. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkill: String!
+}
